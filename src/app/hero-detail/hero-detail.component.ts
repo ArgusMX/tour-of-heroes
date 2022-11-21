38e42221a148cd5a0295dfc49dc53dba3763162f
Volodymyr Sokolov <sokolmx@gmail.com>
2022-11-21 17:15:47 +0200

    Get Data from a Server

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,8 +1,9 @@
-import {Component, OnInit} from '@angular/core';
-import {Hero} from "../hero";
-import {ActivatedRoute} from "@angular/router";
-import {HeroService} from "../hero.service";
-import {Location} from "@angular/common";
+import {Component, Input, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {Location} from '@angular/common';
+
+import {Hero} from '../hero';
+import {HeroService} from '../hero.service';
 
 @Component({
   selector: 'app-hero-detail',
@@ -10,13 +11,13 @@ import {Location} from "@angular/common";
   styleUrls: ['./hero-detail.component.scss']
 })
 export class HeroDetailComponent implements OnInit {
-
-  hero: Hero | undefined;
+  @Input() hero!: Hero;
 
   constructor(
     private route: ActivatedRoute,
     private heroService: HeroService,
-    private location: Location) {
+    private location: Location
+  ) {
   }
 
   ngOnInit(): void {
@@ -31,5 +32,10 @@ export class HeroDetailComponent implements OnInit {
 
   goBack(): void {
     this.location.back();
+  }
+
+  save(): void {
+    this.heroService.updateHero(this.hero)
+      .subscribe(() => this.goBack());
   }
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
-import {Hero} from "../hero";
-import {HeroService} from "../hero.service";
+
+import {Hero} from '../hero';
+import {HeroService} from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,17 +9,34 @@ import {HeroService} from "../hero.service";
   styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
-  heroes: Hero[] = [];
+  heroes!: Hero[];
 
   constructor(private heroService: HeroService) {
   }
 
-  getHeroes() {
-    this.heroService.getHeroes().subscribe(heroes => (this.heroes = heroes));
-  }
-
-
-  ngOnInit(): void {
+  ngOnInit() {
     this.getHeroes();
   }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) {
+      return;
+    }
+    this.heroService.addHero({name} as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
+  }
+
 }
